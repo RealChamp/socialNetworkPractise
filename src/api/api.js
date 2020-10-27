3f887f49e6bef771d5d66b7b15bdd26fa9c4b100
Matthew <matvey.w2015@mail.ru>
2020-10-27 14:51:29 +0400

    isolate requests with DAL

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "aba9ac70-e6ff-4b2d-827c-6e1ddd911830"
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+            return response.data
+        })
+    }
+}
+
